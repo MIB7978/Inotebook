54ec9b60121751d0f19d9827055dcb0af904bf44
codeRocket <ss7727992@gmail.com>
2022-01-23 15:37:37 -0500

    signup

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ connectToMongo()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
-app.use(express.json())
+app.use(express.json()) // middleware
+// app.get('/',(req,res)=>{
+//   res.send('hello')
+// })
 app.use('/api/auth',require('./routes/auth'))
 // app.use('/api/notes',require('./routes/notes'))
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,30 +1,41 @@
-const express  = require('express')
+const express = require("express");
 
-const User = require('../model/User')
-const router = express.Router()
-const { body, validationResult } = require('express-validator');
+const User = require("../model/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-
-//create a user bit not authentication 
-router.post('/',[
-    body('name','enter a valid name ').isLength({ min: 3 }),
-    body('email','enter valid mail').isEmail(),
-    body('password').isLength({ min: 5 })
-],(req,res)=>{
-
-    console.log(req.body);
-    const errors = validationResult(req);
+//create a user for sign up
+router.post(
+  "/",
+  [
+    body("name", "enter a valid name ").isLength({ min: 3 }),
+    body("email", "enter valid mail").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = await validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      // checking user exist or not
+      let user = await User.findOne({ email: req.body.email });
+      if (user) 
+      {
+        return res.status(400).json({ error: "sorry email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json(user);
+    } 
+    catch (error) 
+    {
+      res.status(404).json({ error: "page not found" });
+    }
+  }
+);
 
-    User.create({
-      name: req.body.name,
-      email:req.body.email,
-      password: req.body.password,
-    }).then(user => res.json(user)).catch(err=> {console.log(err)
-    res.json({error:"enter  a unique email",message:err.message})});
-    
-})
-
-module.exports = router
+module.exports = router;
